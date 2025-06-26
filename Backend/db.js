baf910d28e40179e56f78e6debf5c0dfbc3a85c2
Gaurish Naik <dayanand.naik9809@gmail.com>
2025-06-26 19:36:45 +0530

    Removed buffer

--- Backend/db.js
+++ Backend/db.js
@@ -2,12 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
   try {
-    await mongoose.set("bufferCommands", false);
-
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.log(error);
   }
